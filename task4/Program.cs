b4c97b539dd559ad3990b08e7120788b7b4a516f
IULIIA <yulia918@mail.ru>
2022-12-05 01:39:16 +0700

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число ");
+string InpN = Console.ReadLine();
+double N = Convert.ToDouble(InpN);
+
+int i = 1;
+
+while (i <= N)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+        i++;
+}
+
